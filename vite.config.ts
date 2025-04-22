d9a4048c7ef652131f8b962216be2b018a961b9c
Cassandre Pochet <cassandre.pochet@epitech.eu>
2025-04-22 11:20:38 -0400

    feat: :sparkles: Modify build script

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { resolve } from 'path';
+import fs from 'fs';
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/website/" : "/",
@@ -10,5 +12,20 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  buildEnd() {
+    const indexPath = resolve(__dirname, 'dist/index.html');
+    const errorPath = resolve(__dirname, 'dist/404.html');
+    if (fs.existsSync(indexPath)) {
+      fs.copyFileSync(indexPath, errorPath);
+      console.log('✅ Copied index.html to 404.html');
+    }
   },
 }));
