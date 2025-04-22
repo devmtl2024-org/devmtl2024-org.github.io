218f8c9f70b69fcf0c5fc47edce95eafd413bfb9
Cassandre Pochet <cassandre.pochet@epitech.eu>
2025-04-22 14:03:46 -0400

    fix: :bug: Remove last /

--- src/constants/navlinks.ts
+++ src/constants/navlinks.ts
@@ -1,9 +1,9 @@
 const base = import.meta.env.BASE_URL;
 
 export const navLinks = [
-  { name: "About", href: `${base}about` },
-  { name: "Speakers", href: `${base}speakers` },
-  { name: "Schedule", href: `${base}schedule` },
-  { name: "Venue", href: `${base}venue` },
-  { name: "Sponsors", href: `${base}sponsors` },
+  { name: "About", href: `${base}about`.replace(/\/$/, "") },
+  { name: "Speakers", href: `${base}speakers`.replace(/\/$/, "") },
+  { name: "Schedule", href: `${base}schedule`.replace(/\/$/, "") },
+  { name: "Venue", href: `${base}venue`.replace(/\/$/, "") },
+  { name: "Sponsors", href: `${base}sponsors`.replace(/\/$/, "") },
 ];
