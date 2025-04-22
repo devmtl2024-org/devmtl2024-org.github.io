7f62c4cff4925a7b51b88c39a8cb9324876f8b1d
Cassandre Pochet <cassandre.pochet@epitech.eu>
2025-04-22 11:07:08 -0400

    fix: :bug: Fix navlinks

--- src/constants/navlinks.ts
+++ src/constants/navlinks.ts
@@ -1,7 +1,9 @@
+const base = import.meta.env.BASE_URL;
+
 export const navLinks = [
-    { name: "About", href: "/about" },
-    { name: "Speakers", href: "/speakers" },
-    { name: "Schedule", href: "/schedule" },
-    { name: "Venue", href: "/venue" },
-    { name: "Sponsors", href: "/sponsors" },
-  ];
+  { name: "About", href: `${base}about` },
+  { name: "Speakers", href: `${base}speakers` },
+  { name: "Schedule", href: `${base}schedule` },
+  { name: "Venue", href: `${base}venue` },
+  { name: "Sponsors", href: `${base}sponsors` },
+];
