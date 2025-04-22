2d587b76b8d985624350b83d27b42a6c5c1e52b9
Cassandre Pochet <cassandre.pochet@epitech.eu>
2025-04-22 10:35:41 -0400

    fix: :bug: Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import svgr from 'vite-plugin-svgr';
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),svgr()],
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/website/" : "/",
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
