56792eebd1c5b31cb6306fcce9535a53c89088ec
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2025-05-05 18:20:58 -0400

    Upgrade ESLint and fix known issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,16 @@
 import js from "@eslint/js";
-import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
+
+// Fix for the AudioWorkletGlobalScope trailing whitespace issue in globals package
+const browserGlobals = { ...globals.browser };
+if ("AudioWorkletGlobalScope " in browserGlobals) {
+  const value = browserGlobals["AudioWorkletGlobalScope "];
+  delete browserGlobals["AudioWorkletGlobalScope "];
+  browserGlobals["AudioWorkletGlobalScope"] = value;
+}
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -11,7 +19,7 @@ export default tseslint.config(
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: browserGlobals,
     },
     plugins: {
       "react-hooks": reactHooks,
