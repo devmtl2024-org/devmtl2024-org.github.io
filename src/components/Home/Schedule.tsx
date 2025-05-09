f2a863adaf7725cdc916d23cde96ec2ef9f2f034
Cassandre Pochet <cassandre.pochet@epitech.eu>
2025-05-09 11:24:31 -0400

    feat: :sparkles: Update agenda for 2025

--- src/components/Home/Schedule.tsx
+++ src/components/Home/Schedule.tsx
@@ -1,20 +1,8 @@
 import { useTranslation } from "@/hooks/useTranslation";
-import { Speaker } from "@/type/speakers";
-import { loadData } from "@/utils/loadData";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import TalkOverview from "../Talks/TalkOverview";
 
 function Schedule() {
   const { t } = useTranslation();
-  const [speakers, setSpeakers] = useState<Speaker[]>([]);
-
-  useEffect(() => {
-    loadData<Speaker>("speakers", 3, true).then((speakers) => {
-      setSpeakers(speakers);
-    });
-  }, []);
 
   return (
     <section className="bg-gray-100 py-24 px-6 overflow-hidden items-center gap-12 flex flex-col">
@@ -29,35 +17,15 @@ function Schedule() {
             {t({ fr: "Détails du programme", en: "Schedule details" })}
           </h3>
           <h3 className="text-4xl font-semibold text-primary leading-tight mb-4">
-            {t({ fr: "Agenda (2024)", en: "Agenda (2024)" })}
+            {t({ fr: "Agenda (2025)", en: "Agenda (2025)" })}
           </h3>
           <div className="w-16 h-1 bg-primary mx-auto mb-6"></div>
         </motion.div>
 
-        {/* Date Card */}
-        <div className="bg-secondary text-white rounded-lg p-8 shadow-md w-64 text-center">
-          <h4 className="text-2xl font-semibold mb-2">
-            {t({ fr: "27 Novembre", en: "November 27th" })}
-          </h4>
-          <p className="text-xl">{t({ fr: "Mercredi", en: "Wednesday" })}</p>
-        </div>
+        <p className="text-gray-700 text-2xl">
+          {t({ fr: "Bientôt disponible ...", en: "Coming soon ..." })}
+        </p>
       </div>
-
-      <div>
-        {speakers.map((speaker, index) => (
-          <div key={index}>
-            <TalkOverview speaker={speaker} index={index} />
-          </div>
-        ))}
-      </div>
-
-      <Link
-        to="/schedule"
-        className="px-12 py-5 bg-secondary text-white font-medium rounded-md shadow-md hover:bg-secondary-dark uppercase"
-        onClick={() => window.scrollTo({ top: 0 })}
-      >
-        {t({ fr: "Voir plus", en: "See more" })}
-      </Link>
     </section>
   );
 }
--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,39 +1,16 @@
-import TalkRow from "@/components/Talks/TalkRow";
 import { useTranslation } from "@/hooks/useTranslation";
-import { ScheduleSession } from "@/type/schedule";
-import { Speaker } from "@/type/speakers";
-import { groupSpeakersByTime } from "@/utils/groupSpeakers";
-import { loadData } from "@/utils/loadData";
-import { useEffect, useState } from "react";
 
 export default function SchedulePage() {
   const { t } = useTranslation();
-  const [schedule, setSchedule] = useState<ScheduleSession[]>([]);
-
-  useEffect(() => {
-    loadData<Speaker>("speakers").then((speakers) => {
-      const groupedByTime = groupSpeakersByTime(speakers);
-      setSchedule(groupedByTime);
-    });
-  }, []);
 
   return (
-    <div className="container mx-auto px-4 py-16">
-      <h2 className="text-3xl font-medium mb-4 text-secondary text-center">
-        {t({
-          fr: "Programme du 27 Novembre 2024",
-          en: "Schedule of November 27, 2024",
-        })}
-      </h2>
-      <div className="w-16 h-1 bg-secondary mx-auto mb-12"></div>
-
-      {schedule.map((session, index) => (
-        <TalkRow
-          time={session.time}
-          speakers={[session.track1, session.track2]}
-          index={index}
-        />
-      ))}
+    <div className="container mx-auto px-4 py-16 text-center">
+      <h3 className="text-4xl font-semibold text-primary leading-tight mb-4">
+        {t({ fr: "Agenda (2025)", en: "Agenda (2025)" })}
+      </h3>
+      <p className="text-gray-700 text-2xl">
+        {t({ fr: "Bientôt disponible ..", en: "Coming soon .." })}
+      </p>
     </div>
   );
 }
